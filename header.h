9678b0ce079264bfcaa9ed10fb35ed66d67b02a3
Chandni2004 <128141230+Chandni2004@users.noreply.github.com>
2025-04-10 11:45:22 +0530

    Create header.h

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+void EvenOdd(int *);
+void maxTwo(int *, int *);
+void maxThree(int *, int *, int *);
+void maxFour(int *, int *, int *, int *);
+void maxFourShort(int *, int *, int *, int *);
+void secondMaxThree(int *, int *, int *);
+void LeapYear(int *);
+void MagicNumber(int *);
+void LeapLogical(int *);
+void classifyChar(char *);
+#endif
